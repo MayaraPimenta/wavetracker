e8f52fd6eadf552193aef96a6fcc1bc545091168
Mayara Pimenta <mayaralrpimenta@gmail.com>
2022-04-19 19:35:14 -0300

    eslint and initial configuration

---
+++ src/components/Wavetracker.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    wavetracker
+  </div>
+</template>
+
+<script>
+    export default {
+      name: 'Wavetracker',
+    };
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/views/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <Wavetracker />
+  </div>
+</template>
+
+<script>
+import Wavetracker from '@/components/Wavetracker.vue';
+
+export default {
+    name: 'Home',
+
+    components: {
+      Wavetracker
+    },
+  };
+</script>
+
+<style lang="scss" scoped>
+
+</style>
