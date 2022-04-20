7078bbe4509fd3872ecc13443dd79d900e86c26e
Mayara Pimenta <mayaralrpimenta@gmail.com>
2022-04-20 15:38:49 -0300

    added vuex and router plugins

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,14 @@
 <template>
-  <div id="app" />
+  <div id="app">
+    <router-view />
+  </div>
 </template>
 
-<script>
-export default {
-  name: 'App',
-};
-</script>
-
 <style>
-
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+}
 </style>
